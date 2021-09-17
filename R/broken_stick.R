2f2a9103fbc0a3631b2b5eafd32693c43e06941e
ericward-noaa <eric.ward@noaa.gov>
2021-09-17 12:51:59 -0700

    undo ole's dplyr dependency

--- R/broken_stick.R
+++ R/broken_stick.R
@@ -80,7 +80,7 @@ broken_stick <- function(n_obs = 1000,
   # Define the replicates where all of the observations are in one category
   X_cdf_N <- X_cdf * 0
   X_cdf_N[X_cdf > (1-p_one)] <- 1
-  ROWS = apply(X_cdf_N,1,max) %>% unlist()
+  ROWS = apply(X_cdf_N,1,max)
   THESE.rows <- which(ROWS == 1)
 
   # stretch out the middle section so that the proportion between p(x=zero) and p(x=tot_n)
