84596b8f30b795e4c0ce7809de5042360451618a
ericward-noaa <eric.ward@noaa.gov>
2021-10-13 15:56:12 -0700

    chinook data

--- R/data.R
+++ R/data.R
@@ -1,7 +1,18 @@
+#' Data from Satterthwaite, W.H., Ciancio, J., Crandall, E., Palmer-Zwahlen,
+#' M.L., Grover, A.M., O’Farrell, M.R., Anson, E.C., Mohr, M.S. & Garza,
+#' J.C. (2015). Stock composition and ocean spatial distribution from
+#' California recreational chinook salmon fisheries using genetic stock
+#' identification. Fisheries Research, 170, 166–178. The data
+#' genetic data collected from port-based sampling of recreationally-landed
+#' Chinook salmon in California from 1998-2002.
+#'
+#' @format A data frame.
+"chinook"
+
 #' Data from Magnussen, E. 2011. Food and feeding habits of cod (Gadus morhua)
 #' on the Faroe Bank. – ICES Journal of Marine Science, 68: 1909–1917. The data
 #' here are Table 3 from the paper, with sample proportions (columns w) multiplied
-#' by total weight to yield total kg for each sample-diet item combination. Dashes
+#' by total weight to yield total grams (g) for each sample-diet item combination. Dashes
 #' have been replaced with 0s.
 #'
 #' @format A data frame.
